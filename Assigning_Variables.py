1251767e71641f7806b8834e80626a511d44e2c3
Vikas Bisht <bvikastoo@gmail.com>
2023-05-10 20:22:34 +0100

    Print function Practice

--- Assigning_Variables.py
+++ Assigning_Variables.py
@@ -3,3 +3,7 @@ glass_of_Water = str(3)
 #print("I drank", glass_of_Water, "glasses of water today.")
 
 print("I Drank " + (glass_of_Water) + " Glasses of water today")
+
+glass_of_Water = 3
+glass_of_Water = glass_of_Water + 1
+print(glass_of_Water)
