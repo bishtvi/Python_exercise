69fe792f8ccb765e457fffb1f0b45f1abe6b93f1
Vikas Bisht <bvikastoo@gmail.com>
2025-07-21 10:27:37 +0100

    New push

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+
+
+
+for i in range (1,10):
+    count = 0
+    for j in range (1, i+1):
+        if (i%j==0):
+            count = count + 1
+    if(count==2):
+        print(i, end=" ")
+####################
+
+num = int(input("Enter a number: "))
+
+if num%2==0:
+    print("Number is Even")
+else:
+    print("Number is odd")
